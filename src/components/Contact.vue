5f7475362a1743fc7f66155be169a79d31c103d5
Nick Bence <nickbence@helloworld.com>
2019-07-17 10:07:32 -0400

    add font awesome

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="contact">
         <h2 class="page-title">Contact</h2>
+            <a class="email_icon" href="mailto:nick.bence@gmail.com?subject=You%20are%20awesome%20and%20I%20want%20to%20meet%20you."><font-awesome-icon :icon="['fa', 'envelope']" /></a>
+            <a href="//www.linkedin.com/hp/?dnr=Ya6lTB7T8pQbDnKwY5lgrW-08pHEpsBNTvOD" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
+            <a href="//www.facebook.com/nick.bence.1" target="_blank"><font-awesome-icon :icon="['fab', 'facebook-f']" /></a>
+            <a href="//plus.google.com/111917649007675262890/posts" target="_blank"><font-awesome-icon :icon="['fab', 'google']" /></a>
+            <a href="//twitter.com/?lang=en" target="_blank"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
     </div>
 </template>
 
---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+library.add(fas)
+library.add(fab)    
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
