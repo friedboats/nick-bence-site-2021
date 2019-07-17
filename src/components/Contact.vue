31c88640ceefdf40f738204c90e77e331cc29e47
Nick Bence <nickbence@helloworld.com>
2019-07-17 10:23:20 -0400

    contact icons

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,11 +1,12 @@
 <template>
     <div id="contact">
-        <h2 class="page-title">Contact</h2>
-            <a class="email_icon" href="mailto:nick.bence@gmail.com?subject=You%20are%20awesome%20and%20I%20want%20to%20meet%20you."><font-awesome-icon :icon="['fa', 'envelope']" /></a>
-            <a href="//www.linkedin.com/hp/?dnr=Ya6lTB7T8pQbDnKwY5lgrW-08pHEpsBNTvOD" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
-            <a href="//www.facebook.com/nick.bence.1" target="_blank"><font-awesome-icon :icon="['fab', 'facebook-f']" /></a>
-            <a href="//plus.google.com/111917649007675262890/posts" target="_blank"><font-awesome-icon :icon="['fab', 'google']" /></a>
-            <a href="//twitter.com/?lang=en" target="_blank"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+        <div class="icons">
+            <a class="icon_fa icon_email" href="mailto:nick.bence@gmail.com?subject=You%20are%20awesome%20and%20I%20want%20to%20meet%20you."><font-awesome-icon :icon="['fa', 'envelope']" /></a>
+            <a class="icon_fa icon_linkedin" href="//www.linkedin.com/hp/?dnr=Ya6lTB7T8pQbDnKwY5lgrW-08pHEpsBNTvOD" target="_blank"><font-awesome-icon :icon="['fab', 'linkedin-in']" /></a>
+            <a class="icon_fa icon_fb" href="//www.facebook.com/nick.bence.1" target="_blank"><font-awesome-icon :icon="['fab', 'facebook-f']" /></a>
+            <a class="icon_fa icon_google" href="//plus.google.com/111917649007675262890/posts" target="_blank"><font-awesome-icon :icon="['fab', 'google']" /></a>
+            <a class="icon_fa icon_twitter" href="//twitter.com/?lang=en" target="_blank"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+        </div>
     </div>
 </template>
 
@@ -31,4 +32,40 @@
 
 <!-- Add "scoped" attribute to divmit CSS to this component only -->
 <style scoped lang="scss">
+    .icons {
+        display: flex;
+    }
+
+    .icon_fa {
+        width: 50px;
+        margin: 0 10px;
+        color: white;
+        font-size: 30px;
+        transition: color 1s;
+        display: block;
+
+        &:hover {
+            color: white;
+        }
+    }
+
+    .icon_email {
+        color: white;
+    }
+
+    .icon_linkedin {
+        color: #4875B4;
+    }
+
+    .icon_fb {
+        color: #3B5998;
+    }
+
+    .icon_google {
+        color: #DB4437;
+    }
+
+    .icon_twitter {
+        color: #1DA1F2;
+    }
 </style>
