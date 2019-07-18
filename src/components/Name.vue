8c41dc9d7f38762966377729768bc67c579a58f6
Nick Bence <nickbence@helloworld.com>
2019-07-18 00:58:40 -0400

    add name and description

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="name">
         <h2>Nick Bence</h2> 
+        <p>Senior UX<span class="pipe">&</span>Senior Interface Dev</p>
     </div>
 </template>
 
@@ -24,4 +25,23 @@
 
 <!-- Add "scoped" attribute to divmit CSS to this component only -->
 <style scoped lang="scss">
+    .name {
+        h2 {
+            font-size: 25px;
+            margin-bottom: 10px;
+        }
+
+        p {
+            font-size: 23px;
+            font-weight: 500;
+            color: #5ca24b;
+        }
+    }
+        
+    .pipe {
+        font-weight: 900;
+        margin-left: 10px;
+        margin-right: 10px;
+        color: white;
+    }
 </style>
