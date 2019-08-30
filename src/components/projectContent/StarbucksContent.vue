d45d138e15dcfb48734222ac7ecfb92569ba9143
Nick Bence <nickbence@helloworld.com>
2019-08-29 21:58:58 -0400

    add arrow

--- src/components/projectContent/StarbucksContent.vue
+++ src/components/projectContent/StarbucksContent.vue
@@ -3,12 +3,12 @@
         <div class="project-headline"><h2>Starbucks |</h2><h3> Starbucks For Life</h3></div>
         
         <p>“Starbucks For Life” was an interesting campaign for me. This was a really cool idea for a promotion that excited us right off the bat!</p>
-        <p>It was an opportunity for Starbucks customers to win a daily credit for 30 years for one free food or beverage item redeemable at participating Starbucks® stores.</p>
-        <p>What stood out the most in this program's development cycle was the thought process, the build approach and attention to detail. This, in my opinion, is what brought the site to life, making it more than an application for the user, but an experience.</p>
+        <p>It was an opportunity for Starbucks customers to win a daily credit for 30 years for one free food or beverage item redeemable at participating Starbucks® stores. What stood out the most in this program's development cycle was the thought process, the build approach and attention to detail. This, in my opinion, is what brought the site to life, making it more than an application for the user, but an experience.</p>
+        <p>First, I'd like to show you the final site before diving into the 3 sections I mentioned above.</p>
         
-        <h3 class="sub-headline">First... The Site</h3>
+        <h3 class="sub-headline">The Site</h3>
         <slick
-            class="slick-container"
+            class="slick-container margin-btm"
             ref="slick"
             :options="slickOptions">
             <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a>
@@ -18,18 +18,27 @@
             <a href="http://placehold.it/2000x1000"><img src="http://placehold.it/2000x1000" alt=""></a>
         </slick> 
 
-        <h3 class="sub-headline">Thought Process</h3>
-        <p>One of my main roles on this campaign was to collaborate with the client and their 3rd party creative agency to discuss their ideas, concepts and challenges and help provide solutions to those areas.</p>
-        <p>One of the challenges that I’d like to highlight was how to communicate to a user when they earned a single game piece, up to when they collected all 3 game pieces, winning a prize, all while making their experience something special and memorable. Here is what we came up with...</p>
-        <p class="figure-description">Single animation</p>
-        <video class="video-wide callout-frame" controls>
-            <source src="assets/yeti_reveal_sequence.mp4" type="video/mp4">
-        </video>
-        <p class="figure-description">Prize winning animation</p>
-        <video class="video-tall margin-btm" controls>
-            <source src="assets/gilded_icon.mp4" type="video/mp4">
-        </video>
-        <p>Our solution was able to provide the user with the information they needed to understand their experience, organize the content and bring the artwork alive.</p>
+        <p>Now, onto the 3 things that stuck out to me...</p>
+
+        <div class="flex-it">
+            <div class="col-left">
+                <h3 class="sub-headline">Thought Process</h3>
+                <p>One of my main roles on this campaign was to collaborate with the client and their 3rd party creative agency to discuss their ideas, UX concepts and challenges and then help provide solutions to those areas.</p>
+                <p>One of the challenges that we had, that I’d like to highlight, was how we communicated to a user when they earned a single game piece all the way up to when they collected all 3 game pieces, winning one of the main prizes, all while making their experience something special and memorable. Here is what we came up with for the final product of this thought process.</p>
+                <svg class="white-arrow" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 148 143" style="enable-background:new 0 0 148 143;" xml:space="preserve">
+                    <path fill="#FFFFFF" d="M0,0c-0.5-0.5,14,3,14,3s-1.5,102.5,102,79L79,53l10-11l59,46l-55,55l-10-10l39-37C122,96,1.5,131.5,0,0z"/>
+                </svg>
+            </div>
+
+            <div class="col-right"> 
+                <p class="figure-description">Grand Prize Winning Animation</p>
+                <video class="video-tall margin-btm" controls>
+                    <source src="assets/gilded_icon.mp4" type="video/mp4">
+                </video>
+            </div>
+        </div>
+
+        <p>Our solution was to be able to provide the user with the information they needed to understand their experience, organize the content and bring the artwork alive.</p>
         <p>This leads me into the next area I’d like to focus on...</p>
         <h3 class="sub-headline">Build Approach</h3>
         <p>In order to make this experience feel effortless for the user, we decided to develop this campaign in Vue.js as a single page application. This allowed us to provide the user with less load time and smoother transitions, all while keeping their attention and interest in their experience.</p>
@@ -92,5 +101,19 @@
         margin-right: cRems(24px);
         margin-bottom: cRems(12px);
         margin-left: 0;
+    }
+    .flex-it {        
+        .col-left {
+            width: 40%;
+            margin-right: 10%;
+        }
+        .col-right {
+            width: 43%;
+        }
+    }
+    .white-arrow {
+        width: 70%;
+        margin-top: 30px;
+        margin-left: 15%;
     }
 </style>
