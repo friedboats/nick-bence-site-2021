3a93331182268002e9b94bf25fe043702d764337
Nick Bence <nick.bence@gmail.com>
2022-02-27 21:41:20 -0500

    update date

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,46 +1,41 @@
 <template>
-    <div class="footerNav">
-        <p>Nick Bence &copy;2019</p>
-    </div>
+  <div class="footerNav">
+    <p>Nick Bence &copy;2022</p>
+  </div>
 </template>
 
 <script>
-    import {mapState} from 'vuex';
+import { mapState } from "vuex";
 
-    export default {
-        name: 'footerNav',
-        data() {
-            return {
-            }
-        },
-        components: {
-        },
-        methods: {
-        },
-        computed: {
-        },
-        mounted() {
-        }
-    }
+export default {
+  name: "footerNav",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+  computed: {},
+  mounted() {},
+};
 </script>
 
 <!-- Add "scoped" attribute to divmit CSS to this component only -->
 <style lang="scss">
-    .footerNav {
-        width: 100%;
-        text-align: center;
-        background: $colorDarkPurple;
-        position: fixed;
-        bottom: 0;
+.footerNav {
+  width: 100%;
+  text-align: center;
+  background: $colorDarkPurple;
+  position: fixed;
+  bottom: 0;
 
-        p {
-            font-size: cRems(23px);
-            margin-top: cRems(10px);
-            margin-bottom: cRems(10px);
+  p {
+    font-size: cRems(23px);
+    margin-top: cRems(10px);
+    margin-bottom: cRems(10px);
 
-            @include medium {
-                font-size: cRems(15px);
-            }
-        }
+    @include medium {
+      font-size: cRems(15px);
     }
+  }
+}
 </style>
