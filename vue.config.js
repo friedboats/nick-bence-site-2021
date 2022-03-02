33b3ff413e3682b9ad5fdd067a378ea48d084b88
Nick Bence <nick.bence@dynatrace.com>
2022-03-02 14:14:31 -0500

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,9 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/public/dist'
-      : '/',
-    css: {
-        loaderOptions: {
-            sass: {
-              data: `@import "@/scss/app.scss";`
-            }
-          }
-    }
-}
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/scss/app.scss";`,
+      },
+    },
+  },
+};
